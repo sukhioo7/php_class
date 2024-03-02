90a7568f7528719055031d4411157520bc9352b1
sukhioo7 <deepsukhi943@gmail.com>
2024-03-02 11:48:51 +0530

    initall Commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hospital</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index.css">
+  </head>
+  <body>
+    <?php include('navbar.php') ?>
+    <div class="nav-section">
+        <div class="side-img">
+            <img src="img/side_img.png" alt="">
+        </div>
+        <div>
+            <h1>Stay Safe, Stay Healthy</h1>
+            <p>Welcome to our hospital's registration website. We offer a wide range of services, 
+                including inpatient and outpatient care, surgery, and diagnostic testing. To 
+                register for an appointment, please visit our website and provide some basic 
+                information. We also offer online bill pay, medical records requests, and patient 
+                education resources. We hope you find our website helpful.</p>
+            <button class="btn btn-outline-danger">Book Appointment</button>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
