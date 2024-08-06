aa660b4971d36be9775be066d29e5a3a04a86574
sukhioo7 <deepsukhi943@gmail.com>
2024-08-06 15:04:39 +0530

    uploading code

--- action.php
+++ action.php
@@ -4,20 +4,36 @@
 
 if (isset($_POST['patient_register'])){
     
-    $name = $_POST['full_name'];
+    $name = $_POST['full_name']; 
     $age = $_POST['age'];
     $city = $_POST['city'];
     $gender = $_POST['gender'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
     $symptoms = $_POST['symptoms'];
-    
-    $patient_insert = "insert into patients(patient_name,patient_age,patient_city,patient_gender,
-                        patient_email,patient_phone,patient_symptoms) values 
-                        ('$name',$age,'$city','$gender','$email','$phone','$symptoms') ";
 
-    $result = mysqli_query($response,$patient_insert);
-    echo $result;
+    // if (!empty($name) and !empty($age) and !empty($city) and !empty($gender) and !empty($email)
+    //      and !empty($phone) and !empty($symptoms)){
+
+        $check_email = "select * from patients where patient_email='$email'";
+        
+        $patients = mysqli_query($response,$check_email);
+
+        print_r($patients);
+
+        // $patient_insert = "insert into patients(patient_name,patient_age,patient_city,patient_gender,
+        // patient_email,patient_phone,patient_symptoms) values 
+        // ('$name',$age,'$city','$gender','$email','$phone','$symptoms') ";
+
+        // $result = mysqli_query($response,$patient_insert);
+        // header('location:success.php');
+
+    // }else{
+    //     echo 'Please Fill All the Fields.';
+    // }
+
+    
+ 
 }
 
 ?>
--- success.php
+++ success.php
@@ -19,7 +19,7 @@
         <div class="content">
             <h3>Registeration Successfull</h3>
             <p>Your registration has been done. Our Team will reach you shortly with your given email id.</p>
-            <a class="btn btn-outline-success" href="">Back</a>
+            <a class="btn btn-outline-success" href="index.php">Back to home page</a>
         </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
