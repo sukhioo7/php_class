86fccf5ada11ab139fc0a5540c936ebb965f4931
sukhioo7 <deepsukhi943@gmail.com>
2024-09-30 15:53:03 +0530

    updation in form

--- add_blog.php
+++ add_blog.php
@@ -16,6 +16,14 @@
                 <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
                 <label for="floatingInput">Blog Title</label>
             </div>
+            <div class="mb-3">
+                <select class="form-select form-select mb-3" aria-label=".form-select-lg example">
+                    <option selected>Select Category</option>
+                    <option value="1">Weight Loss</option>
+                    <option value="2">Weight Gain</option>
+                    <option value="3">Food</option>
+                </select>
+            </div>
             <div class="form-floating mb-3">
                 <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 200px"></textarea>
                 <label for="floatingTextarea2">Introduction</label>
