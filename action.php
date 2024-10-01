33cccbfab89203accafc3e0f6d4e30993ac2a3a2
sukhioo7 <deepsukhi943@gmail.com>
2024-10-01 15:53:59 +0530

    action and connection file

--- action.php
+++ action.php
@@ -3,8 +3,21 @@
 <!-- ++++++++++++++++++ Add NEW BLOG +++++++++++++++++ -->
 <?php
 
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
+$blog_title = $_POST['blog_title'];
+$introduction = $_POST['introduction'];
+$category  = $_POST['category'];
+$sub_heading1 = $_POST['sub_heading1'];
+$sub_heading2 = $_POST['sub_heading2'];
+$sub_heading3 = $_POST['sub_heading3'];
+$sub_heading4 = $_POST['sub_heading4'];
+$content1 = $_POST['content1'];
+$content2 = $_POST['content2'];
+$content3 = $_POST['content3'];
+$content4 = $_POST['content4'];
+
+
+// echo "<pre>";
+// print_r($_POST);
+// echo "</pre>";
 
 ?>
