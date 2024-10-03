c5a34a95721b94dd1073a69ec4f5fae7e3cc26f7
sukhioo7 <deepsukhi943@gmail.com>
2024-10-03 11:55:41 +0530

    insert operation

--- action.php
+++ action.php
@@ -3,21 +3,40 @@
 <!-- ++++++++++++++++++ Add NEW BLOG +++++++++++++++++ -->
 <?php
 
-$blog_title = $_POST['blog_title'];
-$introduction = $_POST['introduction'];
-$category  = $_POST['category'];
-$sub_heading1 = $_POST['sub_heading1'];
-$sub_heading2 = $_POST['sub_heading2'];
-$sub_heading3 = $_POST['sub_heading3'];
-$sub_heading4 = $_POST['sub_heading4'];
-$content1 = $_POST['content1'];
-$content2 = $_POST['content2'];
-$content3 = $_POST['content3'];
-$content4 = $_POST['content4'];
+if (isset($_POST['add_blog_btn'])){
+    $blog_title = $_POST['blog_title'];
+    $introduction = $_POST['introduction'];
+    $category  = $_POST['category'];
+    $sub_heading1 = $_POST['sub_heading1'];
+    $sub_heading2 = $_POST['sub_heading2'];
+    $sub_heading3 = $_POST['sub_heading3'];
+    $sub_heading4 = $_POST['sub_heading4'];
+    $content1 = $_POST['content1'];
+    $content2 = $_POST['content2'];
+    $content3 = $_POST['content3'];
+    $content4 = $_POST['content4'];
+    
+    if (!empty($blog_title) and !empty($introduction) and !empty($category) and !empty($sub_heading1) 
+    and !empty($sub_heading2) and !empty($sub_heading3) and !empty($sub_heading4) and !empty($content1) 
+    and !empty($content2) and !empty($content3) and !empty($content4)){
 
+        $insert_blog_query = "insert into blogs (blog_title,introduction,category,sub_heading1,sub_heading2,
+        sub_heading3,sub_heading4,content1,content2,content3,content4) values ('$blog_title','$introduction',
+        '$category','$sub_heading1','$sub_heading2','$sub_heading3','$sub_heading4',
+        '$content1','$content2','$content3','$content4')";
 
-// echo "<pre>";
-// print_r($_POST);
-// echo "</pre>";
+        $result = $conn->query($insert_blog_query);
+
+        if ($result){
+            echo "Blog Added Successfully.";
+        }else{
+            echo "Error: ". $conn->error;
+        }
+
+    }else{
+        echo "Please Fill All The Fields.";
+    }
+}
+
 
 ?>
