5d9f367e3af875c1313de8a41a719fda95852ccf
sukhioo7 <deepsukhi943@gmail.com>
2024-10-04 12:26:49 +0530

    fateching blogs

--- blogs.php
+++ blogs.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+<?php include('connection.php'); ?>
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,33 +13,20 @@
 
     <main class="main-div">
       <div class="blog-list">
-        <div class="card mb-3">
-          <img src="https://thumbs.dreamstime.com/b/wooden-pathway-lush-green-tropical-jungle-sunlight-serene-nature-walk-concept-wooden-pathway-lush-green-322646384.jpg" 
-          class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-          </div>
-        </div>
-        <div class="card mb-3">
-          <img src="https://thumbs.dreamstime.com/b/wooden-pathway-lush-green-tropical-jungle-sunlight-serene-nature-walk-concept-wooden-pathway-lush-green-322646384.jpg" 
-          class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-          </div>
-        </div>
-        <div class="card mb-3">
-          <img src="https://thumbs.dreamstime.com/b/wooden-pathway-lush-green-tropical-jungle-sunlight-serene-nature-walk-concept-wooden-pathway-lush-green-322646384.jpg" 
-          class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-          </div>
-        </div>
+        <?php
+            $select_blogs = "select * from blogs";
+
+            $result = $conn->query($select_blogs);
+
+            if ($result->num_rows>0){
+              $blog = $result->fetch_assoc();
+            
+              echo '<pre>';
+              print_r($blog);
+              echo '</pre>';
+            }
+
+        ?>
         <div class="card mb-3">
           <img src="https://thumbs.dreamstime.com/b/wooden-pathway-lush-green-tropical-jungle-sunlight-serene-nature-walk-concept-wooden-pathway-lush-green-322646384.jpg" 
           class="card-img-top" alt="...">
