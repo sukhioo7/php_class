3cc17c3b8f2a82fe52d0b404e1c3fefd3dc21138
sukhioo7 <deepsukhi943@gmail.com>
2024-10-07 11:27:27 +0530

    card code

--- blogs.php
+++ blogs.php
@@ -19,23 +19,23 @@
             $result = $conn->query($select_blogs);
 
             if ($result->num_rows>0){
-              $blog = $result->fetch_assoc();
-            
-              echo '<pre>';
-              print_r($blog);
-              echo '</pre>';
+              while ($blog = $result->fetch_assoc()){
+        ?>
+                <div class="card mb-3">
+                  <img src="https://thumbs.dreamstime.com/b/wooden-pathway-lush-green-tropical-jungle-sunlight-serene-nature-walk-concept-wooden-pathway-lush-green-322646384.jpg" 
+                  class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  </div>
+                </div>
+        <?php
+              }
             }
 
         ?>
-        <div class="card mb-3">
-          <img src="https://thumbs.dreamstime.com/b/wooden-pathway-lush-green-tropical-jungle-sunlight-serene-nature-walk-concept-wooden-pathway-lush-green-322646384.jpg" 
-          class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-          </div>
-        </div>
+
       </div>
       <div class="filter-list">
         bye
