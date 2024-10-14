51c94d896aff8002f1791fde6f71bdb61d9baaa4
sukhioo7 <deepsukhi943@gmail.com>
2024-10-14 12:51:57 +0530

    lestest code

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
           object-position: 0 0px;
       }
     </style>
+    <link rel="stylesheet" href="css/footer.css">
   </head>
   <body>
     <?php include('navbar.php') ?>
@@ -47,6 +48,7 @@
         ?>
       </div>
     </main>
+    <?php include('footer.php') ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>
